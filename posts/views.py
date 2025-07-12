8efb8b2b1f8315809e221b5e1e0e873ffb782aa7
Abduolimov03 <asadbekabduolimov33@gmail.com>
2025-07-12 10:09:46 +0500

    second commit

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category,News
 
 
 def index(request):
     categories = Category.objects.all()
-    return render(request, 'index.html', {'categories':categories})
+    first_news = []
+    for category in categories:
+        category_first_post = News.objects.filter(category=category).order_by('-created_at').first()
+        if category_first_post is not None:
+            first_news.append(category_first_post)
+    if len(first_news) < 4:
+        news = News.objects.all()
+        first_news.extend(news[len(news)-4:len(news)-len(first_news)])
+
+    news = News.objects.all().order_by('-id')
+
+
+    return render(request, 'index.html', {'categories':categories, 'first_news':first_news, 'news':news})
